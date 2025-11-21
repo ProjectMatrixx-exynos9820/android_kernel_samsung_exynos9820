5fcec8ef1a2cc449cedc5872391b70852ee1769e
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2025-11-21 20:54:04 +0200

    treewide: SUSFS Inline Hooks
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>
    Co-authored-by: JackAltman <JackA1ltman@users.noreply.github.com>
    Co-authored-by: momenabdulrazek <momen.abdelrazek33@gmail.com>
    [Flopster101: guard with KSU_SUSFS instead.]
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -717,6 +717,10 @@ error:
  * This function implements a generic ability to update ruid, euid,
  * and suid.  This allows you to implement the 4.4 compatible seteuid().
  */
+#ifdef CONFIG_KSU_SUSFS
+extern int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
+#endif
+
 SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 {
 	struct user_namespace *ns = current_user_ns();
@@ -728,7 +732,13 @@ SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 	kruid = make_kuid(ns, ruid);
 	keuid = make_kuid(ns, euid);
 	ksuid = make_kuid(ns, suid);
-	
+
+#ifdef CONFIG_KSU_SUSFS
+	if (ksu_handle_setresuid(ruid, euid, suid)) {
+		pr_info("Something wrong with ksu_handle_setresuid()/n");
+	}
+#endif
+
 	if ((ruid != (uid_t) -1) && !uid_valid(kruid))
 		return -EINVAL;
 
