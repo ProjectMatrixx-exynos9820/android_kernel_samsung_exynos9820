bd0a11d21cf0be321485ffa35346b9478ffff49c
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2025-11-30 18:50:55 -0500

    drivers: sukisu: Sync with latest SukiSU-Ultra source

--- drivers/sukisu/kernel/kernel_umount.c
+++ drivers/sukisu/kernel/kernel_umount.c
@@ -1,6 +1,5 @@
 #include <linux/sched.h>
 #include <linux/slab.h>
-#include <linux/string.h>
 #include <linux/task_work.h>
 #include <linux/version.h>
 #include <linux/cred.h>
@@ -57,7 +56,7 @@ extern bool susfs_is_log_enabled;
 #endif // #ifdef CONFIG_KSU_SUSFS
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) ||						   \
-    defined(KSU_HAS_PATH_UMOUNT)
+	defined(KSU_HAS_PATH_UMOUNT)
 extern int path_umount(struct path *path, int flags);
 static void ksu_umount_mnt(const char *__never_use_mnt, struct path *path,
 			   int flags)
@@ -84,10 +83,10 @@ static void ksu_sys_umount(const char *mnt, int flags)
 }
 
 #define ksu_umount_mnt(mnt, __unused, flags)                                   \
-    ({                                                                     \
-	    path_put(__unused);                                            \
-	    ksu_sys_umount(mnt, flags);                                    \
-     })
+	({                                                                     \
+		path_put(__unused);                                            \
+		ksu_sys_umount(mnt, flags);                                    \
+	})
 
 #endif
 
@@ -118,17 +117,6 @@ static void umount_tw_func(struct callback_head *cb)
 {
 	struct umount_tw *tw = container_of(cb, struct umount_tw, cb);
 	const struct cred *saved = NULL;
-	const char **exclude_paths;
-	u32 exclude_count = 0;
-
-	exclude_paths = kmalloc_array(MAX_PATHS, sizeof(const char *), GFP_KERNEL);
-	if (!exclude_paths) {
-		if (tw->old_cred)
-			put_cred(tw->old_cred);
-		kfree(tw);
-		return;
-	}
-
 	if (tw->old_cred) {
 		saved = override_creds(tw->old_cred);
 	}
@@ -136,28 +124,12 @@ static void umount_tw_func(struct callback_head *cb)
 	struct mount_entry *entry;
 	down_read(&mount_list_lock);
 	list_for_each_entry(entry, &mount_list, list) {
-		// Check for duplicates
-		bool is_duplicate = false;
-		u32 i;
-		for (i = 0; i < exclude_count; i++) {
-			if (exclude_paths[i] && !strcmp(exclude_paths[i], entry->umountable)) {
-				is_duplicate = true;
-				break;
-			}
-		}
-		// Add to exclude list if not duplicate and not full
-		if (!is_duplicate && exclude_count < MAX_PATHS) {
-			exclude_paths[exclude_count] = entry->umountable;
-			exclude_count++;
-		}
-        pr_info("%s: unmounting: %s flags 0x%x\n", __func__, entry->umountable, entry->flags);
+		pr_info("%s: unmounting: %s flags 0x%x\n", __func__, entry->umountable, entry->flags);
 		try_umount(entry->umountable, entry->flags);
 	}
 	up_read(&mount_list_lock);
 
-	ksu_umount_manager_execute_all(tw->old_cred, exclude_paths, exclude_count);
-
-	kfree(exclude_paths);
+	ksu_umount_manager_execute_all(tw->old_cred);
 
 	if (saved)
 		revert_creds(saved);
