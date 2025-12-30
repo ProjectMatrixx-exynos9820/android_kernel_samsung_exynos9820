f372bcb9576a2d7d0a3a3f491bd8fccd9b04a3df
Flopster101 <nahuelgomez329@gmail.com>
2025-12-30 00:10:53 -0500

    block: zram: Implement automatic ZRAM size and set to be roughly 50% of RAM

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1800,20 +1800,36 @@ static ssize_t disksize_store(struct device *dev,
 		goto out_unlock;
 	}
 
-#ifndef CONFIG_ZRAM_SIZE_OVERRIDE
-	disksize = PAGE_ALIGN(disksize);
-	if (!disksize)
-		return -EINVAL;
-#else
-	if (!zram_size_set_once) {
-		disksize = (u64)SZ_1 * CONFIG_ZRAM_SIZE_OVERRIDE;
-		zram_size_set_once = true;
-		pr_info("Overriding zram size to %li", disksize);
-	} else {
-		disksize = PAGE_ALIGN(disksize);
-		if (!disksize)
-			return -EINVAL;
+#ifdef CONFIG_ZRAM_SIZE_AUTO
+	{
+		/*
+		 * Dynamic ZRAM Size Detection (50% of RAM):
+		 * totalram_pages() returns usable pages.
+		 * Thresholds based on physical RAM variants:
+		 * 6GB Dev: Max 6144 MB -> Usable ~5800 MB -> ZRAM 3GB
+		 * 8GB Dev: Max 8192 MB -> Usable ~7600 MB -> ZRAM 4GB
+		 * 12GB Dev: Max 12288 MB -> Usable ~11500 MB -> ZRAM 6GB
+		 */
+		unsigned long total_ram_mb =
+		totalram_pages * (PAGE_SIZE / 1024) / 1024;
+
+		if (total_ram_mb > 10000) {
+			disksize = 6ULL * SZ_1G;
+			pr_info("Detected 12GB RAM variant (usable: %lu MB), setting ZRAM to 6GB (50%%)",
+					total_ram_mb);
+		} else if (total_ram_mb > 7000) {
+			disksize = 4ULL * SZ_1G;
+			pr_info("Detected 8GB RAM variant (usable: %lu MB), setting ZRAM to 4GB (50%%)",
+					total_ram_mb);
+		} else {
+			disksize = 3ULL * SZ_1G;
+			pr_info("Detected 6GB RAM variant (usable: %lu MB), setting ZRAM to 3GB (50%%)",
+					total_ram_mb);
+		}
 	}
+#elif defined(CONFIG_ZRAM_SIZE_OVERRIDE)
+	disksize = (u64)SZ_1 * CONFIG_ZRAM_SIZE_OVERRIDE;
+	pr_info("Overriding zram size to %llu", disksize);
 #endif
 	if (!zram_meta_alloc(zram, disksize)) {
 		err = -ENOMEM;
