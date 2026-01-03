7f6a5df6d6f262f376cc2661b916889148d95574
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2026-01-03 00:08:31 -0500

    drivers: Add support for my FreeRunnerKernelManager

--- drivers/kernelsu/kernel/apk_sign.c
+++ drivers/kernelsu/kernel/apk_sign.c
@@ -315,5 +315,12 @@ module_param_cb(ksu_debug_manager_appid, &expected_size_ops,
 
 bool is_manager_apk(char *path)
 {
-	return check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH);
+	if (check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH)) {
+		return true;
+	}
+	if (check_v2_signature(path, FREERUNNER_NEXT_MANAGER_SIZE, FREERUNNER_NEXT_MANAGER_HASH)) {
+		return true;
+	}
+
+	return false;
 }
