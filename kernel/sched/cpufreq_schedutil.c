fc18c002cee6c915ea80672970859849b16435d0
FelcoX <godxyukarzzyt@gmail.com>
2026-05-06 03:54:20 +0000

    Fixup: sched/cpufreq: remove rq-level uclamp_min floor
    sugov_get_util() and sugov_next_freq_shared() were applying rq-level
    uclamp aggregation via uclamp_util_with(rq, *util, NULL) after
    boosted_cpu_util().
    
    Signed-off-by: FelcoX <parbindarsingh@outlook.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -433,9 +433,6 @@ skip_betting:
 static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
 {
 	unsigned long max_cap;
-#ifdef CONFIG_UCLAMP_TASK
-	struct rq *rq = cpu_rq(cpu);
-#endif
 
 	max_cap = arch_scale_cpu_capacity(NULL, cpu);
 
@@ -447,9 +444,6 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
 	*util = min(*util, max_cap);
 	*max = max_cap;
 
-#ifdef CONFIG_UCLAMP_TASK
-   	*util = uclamp_util_with(rq, *util, NULL);
-#endif	
 }
 
 #ifdef CONFIG_SCHED_KAIR_GLUE
