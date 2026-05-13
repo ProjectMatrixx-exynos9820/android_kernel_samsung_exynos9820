c52326f77f0aad33ed6be10cb3b4cff5132dc239
FelcoX <godxyukarzzyt@gmail.com>
2026-05-13 09:11:31 +0000

    sched/cpufreq_schedutil: use UCLAMP_MIN in sugov_need_slack_timer()
    * Use uclamp[UCLAMP_MIN].value instead of schedtune_cpu_boost()
    
    Signed-off-by: FelcoX <parbindarsingh@outlook.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1224,7 +1224,7 @@ static int sugov_need_slack_timer(unsigned int cpu)
 	struct sugov_cpu *sg_cpu = &per_cpu(sugov_cpu, cpu);
 	struct sugov_exynos *sg_exynos = &per_cpu(sugov_exynos, cpu);
 
-	if (schedtune_cpu_boost(cpu))
+	if (READ_ONCE(cpu_rq(cpu)->uclamp[UCLAMP_MIN].value))
 		return 0;
 
 	if (sg_cpu->util > sg_exynos->min &&
